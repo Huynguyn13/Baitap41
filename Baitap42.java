04d660722747105090a94c6199bd9d0b8b85d0a4
Huynguyn13 <97729515+Huynguyn13@users.noreply.github.com>
2022-02-18 16:31:37 +0700

    Add files via upload

---
+++ Baitap42.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package baitap42;
+
+/**
+ *
+ * @author admin
+ */
+import java.util.Scanner;
+public class Baitap42 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+       int b=sc.nextInt();
+        System.out.println("a+b="+(a+b));
+       System.out.println("a-b="+(a-b));
+       System.out.println("a*b="+(a*b));
+       System.out.println("a/b="+(a/b));
+       System.out.println("a%b="+(a%b));
+       System.out.println("a^b="+(a^b));
+    
+    
+    
+    }
+    
+}
---
+++ Bsitap43.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bsitap43;
+
+/**
+ *
+ * @author admin
+ */
+import java.util.Scanner;
+public class Bsitap43 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner sc= new Scanner(System.in);
+        Double a=sc.nextDouble();
+       Double b=sc.nextDouble();
+       s
+        System.out.println("a+b="+(a+b));
+       System.out.println("a-b="+(a-b));
+       System.out.println("a*b="+(a*b));
+       System.out.println("a/b="+(a/b));
+       System.out.println("a%b="+(a%b));
+       System.out.println(a+"^"+y"="z);
+    }
+    
+}
